936d0c3cec38a5664e67a23d6c06c2c879f57def
Fairooz Nur Tasnim <fairooz.n.tasnim@gmail.com>
2026-08-03 18:05:08 +0600

    Update social media links and icons

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/* =========================
+   MOBILE MENU
+========================= */
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+if (menuToggle && navMenu) {
+
+    menuToggle.addEventListener("click", () => {
+
+        navMenu.classList.toggle("mobile-active");
+
+    });
+
+}
